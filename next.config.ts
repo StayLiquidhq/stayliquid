1443a948dc64c3c5e43ab5da1f4e35be385e0d83
clement <clementakhimien25@gmail.com>
2025-09-30 23:17:32 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
     HELIUS_API_KEY: process.env.HELIUS_API_KEY,
     WEBHOOK_URL: process.env.WEBHOOK_URL,
     HELIUS_WEBHOOK_ID: process.env.HELIUS_WEBHOOK_ID,
-    DEV_WALLET_PRIVATE_KEY: process.env.DEV_WALLET_PRIVATE_KEY,
     PAYOUT_PRIVATE_KEY: process.env.PAYOUT_PRIVATE_KEY,
     USDC_MINT: process.env.USDC_MINT,
   },
